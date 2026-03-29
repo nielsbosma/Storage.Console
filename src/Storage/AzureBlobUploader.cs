75180724b9c452fd01cdf8d35e925f781bbd6b18
nielsbosma <niels@bosmainteractive.se>
2026-03-29 16:22:05 +0200

    Set proper content type on blob upload
    Detect MIME type from file extension so files display inline in the browser
    instead of triggering a download.

--- src/Storage/AzureBlobUploader.cs
+++ src/Storage/AzureBlobUploader.cs
@@ -1,6 +1,6 @@
 using System.Diagnostics;
-using System.Text.Json;
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace Storage.Console.Storage;
 
@@ -22,13 +22,46 @@ public static class AzureBlobUploader
 
         var blobClient = new BlobClient(blobUri);
 
+        var contentType = GetContentType(blobName);
+        var uploadOptions = new BlobUploadOptions
+        {
+            HttpHeaders = new BlobHttpHeaders { ContentType = contentType }
+        };
+
         await using var fileStream = File.OpenRead(filePath);
-        await blobClient.UploadAsync(fileStream, overwrite: true, cancellationToken);
+        await blobClient.UploadAsync(fileStream, uploadOptions, cancellationToken);
 
         var readSasToken = await GenerateSasTokenAsync(
             accountName, containerName, permissions: "r", expires, cancellationToken);
 
         return $"https://{accountName}.blob.core.windows.net/{containerName}/{blobName}?{readSasToken}";
+    }
+
+    private static string GetContentType(string fileName)
+    {
+        var ext = Path.GetExtension(fileName).ToLowerInvariant();
+        return ext switch
+        {
+            ".jpg" or ".jpeg" => "image/jpeg",
+            ".png" => "image/png",
+            ".gif" => "image/gif",
+            ".webp" => "image/webp",
+            ".svg" => "image/svg+xml",
+            ".pdf" => "application/pdf",
+            ".json" => "application/json",
+            ".xml" => "application/xml",
+            ".html" or ".htm" => "text/html",
+            ".css" => "text/css",
+            ".js" => "application/javascript",
+            ".txt" or ".log" or ".md" => "text/plain",
+            ".csv" => "text/csv",
+            ".zip" => "application/zip",
+            ".gz" => "application/gzip",
+            ".tar" => "application/x-tar",
+            ".mp4" => "video/mp4",
+            ".mp3" => "audio/mpeg",
+            _ => "application/octet-stream"
+        };
     }
 
     private static async Task<string> GenerateSasTokenAsync(
